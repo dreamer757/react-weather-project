47110a4d98308caf367b18fd0c4666925c2866f0
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-07-19 21:53:56 -0400

    added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Courtney Jones and is{" "}
+        <a
+          href="https://github.com/dreamer757/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
