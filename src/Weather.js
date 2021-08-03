daef310820e819bfcf0774a0cc735259b65026b1
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-02 23:03:28 -0400

    fixed api call

--- src/Weather.js
+++ src/Weather.js
@@ -30,7 +30,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "63526e0bd92561567cb0c7fc56e7f18c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
