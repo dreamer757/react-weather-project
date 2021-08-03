088353fb6208925b84765860777e468b6ec2f7a3
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-02 22:02:51 -0400

    fixed wind

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -30,7 +30,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <ul>
             <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/hr</li>
+            <li>Wind: {props.data.wind} km/hr</li>
           </ul>
         </div>
       </div>
