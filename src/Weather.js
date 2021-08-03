cd07e75418aae13be985fb3126a68860a16edca7
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-02 21:16:29 -0400

    added custom icons

--- src/Weather.js
+++ src/Weather.js
@@ -51,10 +51,7 @@ export default function Weather(props) {
           <div className="col-6">
             <div className="clearfix">
               <div className="float-left">
-                <WeatherIcon
-                  code={props.data.icon}
-                  alt={props.data.description}
-                />
+                <WeatherIcon code={props.data.icon} />
               </div>
 
               <div className="float-left">
