1cd63ba9caa70c4ad02f8f7053d2a0b944cf6a84
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-08-04 23:32:10 -0400

    fixed errors

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,16 +2,33 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function weatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+    return days[day];
+  }
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.dt} </div>
-      <WeatherIcon code={forecast[0].weather[0].icon} size={36} />
+      <div className="WeatherForecast-day">{day()} </div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {forecast[0].temp.max}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {forecast[0].temp.min}°
+          {minTemperature()}
         </span>
       </div>
     </div>
